05e65af890a82584e2d8db317965350d50c9c8bd
Subhadra Gope <subhadragope27@gmail.com>
2024-01-25 03:58:48 +0530

    created components

---
+++ src/Components/ToDoItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ToDoItem = () => {
+  return (
+    <div>ToDoItem</div>
+  )
+}
---
+++ src/Components/ToDos.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ToDos = () => {
+  return (
+    <div>ToDos</div>
+  )
+}
